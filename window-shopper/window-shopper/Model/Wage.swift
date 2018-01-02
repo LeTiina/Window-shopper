5a96287cca684ab03b6aa5cea7b20e2954966d10
TiinaAn <tiina@meapu.com>
2018-01-02 15:40:33 +0200

    2018 commit

---
+++ window-shopper/window-shopper/Model/Wage.swift
@@ -0,0 +1,14 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Tiina Antikainen on 12/31/17.
+//  Copyright © 2017 Tiina Antikainen. All rights reserved.
+//
+
+import Foundation
+class Wage {
+    class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
+        return Int(ceil(price / wage))
+    }
+}
--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -11,6 +11,16 @@ import UIKit
 @IBDesignable
 class CurrencyTxtField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 0.4361501338)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+    
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
